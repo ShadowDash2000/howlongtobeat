5b82404c75f3c343524874ff1e8d00730ebfb5a0
shadod <scout0501@gmail.com>
2026-03-29 04:37:55 +0300

    refactor: updated endpoint paths

--- constants.go
+++ constants.go
@@ -6,9 +6,9 @@ const (
 	// hltbBaseURL is the base URL for the HowLongToBeat.
 	hltbBaseURL = "https://howlongtobeat.com"
 	// hltbSearchEndpoint is the default endpoint for the HowLongToBeat search API.
-	hltbSearchEndpoint = "/api/finder"
+	hltbSearchEndpoint = "/api/find"
 	// hltbTokenURL is the URL to retrieve the token for the HowLongToBeat API.
-	hltbTokenURL = "https://howlongtobeat.com/api/finder/init"
+	hltbTokenURL = "https://howlongtobeat.com/api/find/init"
 	// hltbGameURL is the base URL for the HowLongToBeat game API.
 	hltbGameURL = "https://howlongtobeat.com/game"
 	// defaultRequestTimeout is the default timeout for outgoing requests, we wait up to 30 seconds.
